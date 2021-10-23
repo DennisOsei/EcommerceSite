32e426fc02986dd492bd5784d0c72d8961629b25
Dennis <dennisoseitimss1@gmail.com>
2021-10-22 23:17:59 -0400

    changed settings for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#u%t-rx_pc&javly^+091w2s4v1zo%o8q6k1a4+wh&9@scr#^r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-env1.eba-ieauyp2u.us-east-1.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["mydjangoenv.eba-hif4bxwh.us-east-1.elasticbeanstalk.com"]
 
 
 # Application definition
